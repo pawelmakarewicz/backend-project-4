c50afb5c7292fb6c623acf8a80122909ce21ba4a
Pawel_Makarewicz <pawelmakarewicz897@gmail.com>
2024-10-06 15:53:00 +0200

    add commander

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log("HELLO WORLD!!!!")
+import { Command } from 'commander';
+import { pageLoader } from "../lib/pageLoader.js";
+
+const program = new Command();
+program
+  .description('Page loader utility.')
+  .version('1.0.0')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .arguments('<url>')
+  .action((url, options) => {
+    pageLoader(url , options.output);
+  });
+program.parse();
+ 
