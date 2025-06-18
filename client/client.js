9a999958bf36200544519a458da883134c8e74c6
Pawel_Makarewicz <pawelmakarewicz897@gmail.com>
2025-06-18 08:06:31 +0200

    chord: refactor pageLoader

--- client/client.js
+++ client/client.js
@@ -2,8 +2,14 @@ import axios from 'axios';
 
 const get = (url, params = {}) => axios.get(url, { params }).then((response) => response.data);
 
+const getBinary = (url, params = {}) => axios.get(url, {
+  params,
+  responseType: 'arraybuffer', // This handles binary data
+}).then((response) => response.data);
+
 const httpClient = {
   get,
+  getBinary,
 };
 
 export default httpClient;
