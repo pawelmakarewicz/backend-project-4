933a3c7a6af62e6cd5ce78d50eb6e6d7cddf742b
Pawel_Makarewicz <pawelmakarewicz897@gmail.com>
2025-11-09 12:34:00 +0100

    feat: add debug log to client.js

--- client/client.js
+++ client/client.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const get = (url, params = {}) => axios.get(url, { params }).then((response) => response.data);
+console.log("dummy client!!!");
 
 const httpClient = {
   get,
