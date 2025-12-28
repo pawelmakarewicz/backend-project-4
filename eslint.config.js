bea7913aae678d58d3080a4bacedf5d4f1f66ff7
Pawel_Makarewicz <pawelmakarewicz897@gmail.com>
2025-12-28 22:08:06 +0100

    fix: remove lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,8 @@
 import globals from 'globals';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
 
 export default [
   {
@@ -33,7 +23,6 @@ export default [
       ...importPlugin.configs.recommended.rules,
     },
   },
-  ...compat.extends('airbnb-base'),
   {
     rules: {
       'no-underscore-dangle': [
