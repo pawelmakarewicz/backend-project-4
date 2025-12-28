7de8302628c695be408a4076e431a1a879bc566c
Pawel_Makarewicz <pawelmakarewicz897@gmail.com>
2025-12-28 21:04:37 +0100

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,7 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-  },
-  pluginJs.configs.recommended,
-  {
-    plugins: { 
-      import: importPlugin 
-    },
-    rules: {
-      'no-console': 'off',
-      'no-underscore-dangle': ['error', {
-        allow: ['__filename', '__dirname'],
-      }],
-      'import/extensions': ['error', 'ignorePackages', {
-        js: 'always',
-      }],
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-extraneous-dependencies': 'off',
-    },
-  },
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+]);
